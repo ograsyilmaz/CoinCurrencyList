6524449c8a85e42a4f2878b1c45bdcaeae78b545
ograsyilmaz <yilmazogras@outlook.com.tr>
2022-02-10 14:44:29 +0300

    axios   add main

--- src/main.js
+++ src/main.js
@@ -1,8 +1,10 @@
-import Vue from 'vue'
-import App from './App.vue'
+import Vue from "vue";
+import App from "./App.vue";
+import axiosIns from "./lib/axios";
 
-Vue.config.productionTip = false
+Vue.config.productionTip = false;
 
+Vue.use(axiosIns);
 new Vue({
-  render: h => h(App),
-}).$mount('#app')
+  render: (h) => h(App),
+}).$mount("#app");
