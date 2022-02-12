5ef5dad8127d38280976c40bd0668dc29080e6ec
ograsyilmaz <yilmazogras@outlook.com.tr>
2022-02-12 04:03:44 +0300

    view list box select

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,24 +1,72 @@
 <template>
   <div class="hello">
-    <p>{{ msg }}</p>
+    <!-- <p>{{ get_Biance_Colletion_Data }}</p> -->
+    <p></p>
+    <!-- <b-form-select
+      v-model="selected"
+      :options="get_Biance_Cerruncy_Sembol_List"
+    ></b-form-select>
+    {{ selected }} -->
+    {{ get_Biance_Colletion_Data }}
+    <label for="input-with-list">Input with datalist</label>
+    <b-form-input
+      class="mt-2"
+      v-model="selected"
+      list="input-list"
+      id="input-with-list"
+      @change="hepsiburada"
+    ></b-form-input>
+    <b-form-datalist
+      id="input-list"
+      :options="get_Biance_Cerruncy_Sembol_List"
+    ></b-form-datalist>
   </div>
 </template>
 
 <script>
+import { mapGetters, mapActions } from "vuex";
+
 export default {
   name: "HelloWorld",
+  components: {},
   data() {
     return {
       msg: [],
+      selected: null,
     };
   },
+  computed: {
+    ...mapGetters([
+      "get_Biance_Cerruncy_Sembol_List",
+      "get_Biance_Colletion_Data",
+    ]),
+  },
   created() {
-    this.$http
-      .get("/ticker/24hr")
-      .then((response) => {
-        this.msg=response.data
-        console.log(this.msg)
-      });
+    this.getBianceCurrencyRateSembol();
+  },
+  beforeCreate() {},
+  watch: {
+    // selected(value) {
+    //   // localStorage.setItem("CoinCurrency", value);
+    //   // this.getfilterBianceCurrenctByRateSembol(value);
+    // },
+  },
+
+  methods: {
+    ...mapActions([
+      "getBianceCurrencyRateSembol",
+      "getfilterBianceCurrenctByRateSembol",
+    ]),
+    hepsiburada(x){
+      localStorage.setItem("CoinCurrency", x);
+      this.getfilterBianceCurrenctByRateSembol(x);
+      console.log(x +" test data")
+    }
+  },
+  mounted() {
+    if (localStorage.CoinCurrency) {
+      this.selected = localStorage.CoinCurrency;
+    }
   },
 };
 </script>
