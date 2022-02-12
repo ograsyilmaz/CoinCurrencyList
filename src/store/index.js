dddc6323c9a17bfff4841289b64242409343c137
ograsyilmaz <yilmazogras@outlook.com.tr>
2022-02-12 04:03:58 +0300

    project  add

--- src/store/index.js
+++ src/store/index.js
@@ -1,8 +1,58 @@
-import { createStore } from "vuex";
+import Vue from "vue";
+import Vuex from "vuex";
 
-export default createStore({
-  state: {},
-  getters: {},
-  mutations: {},
-  actions: {},
+Vue.use(Vuex);
+
+const state = {
+  bianceData: [],
+  bianceCerruncyList: [],
+  selectCurruncySymbol: "",
+};
+const mutations = {
+  set_Biance_Collection_Data(state, paylod) {
+    state.bianceData.push(paylod);
+  },
+  set_BianceCerruncyList(state, paylod) {
+    state.bianceCerruncyList = paylod;
+  },
+};
+const getters = {
+  get_Biance_Colletion_Data(state) {
+    return state.bianceData;
+  },
+  get_Biance_Cerruncy_Sembol_List(state) {
+    return state.bianceCerruncyList;
+  },
+};
+const actions = {
+  async getBianceCurrencyRateSembol({ commit }) {
+    const dataList = [];
+    await this._vm.$http.get("/ticker/24hr").then((response) => {
+      response.data.map((data) => {
+        const value = {
+          text: data.symbol,
+          value: data.symbol,
+        };
+        dataList.push(value);
+      });
+    });
+    commit("set_BianceCerruncyList", dataList);
+  },
+
+  async getfilterBianceCurrenctByRateSembol({ commit }, value) {
+    let filterData = "";
+    await this._vm.$http.get("/ticker/24hr").then((response) => {
+      filterData = response.data.filter((x) => {
+        return x.symbol == value;
+      });
+    });
+    commit("set_Biance_Collection_Data", filterData);
+  },
+};
+
+export default new Vuex.Store({
+  state,
+  getters,
+  actions,
+  mutations,
 });
